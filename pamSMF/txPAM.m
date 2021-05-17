b0b05d066c47104922eb63b97ce6c5a4a49499b8
Alex Salois <asalois.scholar@gmail.com>
2021-05-17 10:46:46 -0600

    co-sim works

--- pamSMF/txPAM.m
+++ pamSMF/txPAM.m
@@ -7,13 +7,13 @@
 nb = 2^patternLength;
 msg = randi([0 M-1],1,nb);
 tx = pammod(msg,M);
-tx = rectpulse(tx,2^pointsPerBit)
+tx = rectpulse(tx,2^pointsPerBit);
 
 noPoints = 2^noSamples;
 OutNode{1}.noSignals = 1;
 OutNode{1}.Signal.noPoints = noPoints;
 OutNode{1}.Signal.samples = complex(tx');
-% OutNode{1}.Signal.sigma = complex(zeros(1,noPoints));
+OutNode{1}.Signal.sigma = complex(zeros(1,noPoints));
 OutNode{1}.Signal.tStep = timeStep;
 OutNode{1}.Signal.startTime = 0;
 OutNode{1}.Signal.bitRate = bitRate;
