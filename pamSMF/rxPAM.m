4f3f8e126cd1ef8f1dec282c1ba7821c1958b0d6
Alex Salois <alexandersalois@msu.montana.edu>
2021-06-17 12:14:03 -0600

    change mat files name to include length

--- pamSMF/rxPAM.m
+++ pamSMF/rxPAM.m
@@ -3,6 +3,6 @@
 % Montana State University
 % Electrical & Computer Engineering Department
 % Created by Alexander Salois
-
-saveName = sprintf('pam_snr_%02d_%04d',OSNR,runNum);
+fiberLength = 10*fiberLength;
+saveName = sprintf('pam_snr_%02d_len_%04d_%04d',OSNR,fiberLength,runNum);
 save(saveName)
