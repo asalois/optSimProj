44e2a0fcc00384d32531281f307e6fbf96052400
Alexander Salois <asalois.scholar@gmail.com>
2021-06-17 12:07:02 -0600

    did some adjusting for new file names

--- pamSMF/doDSP.m
+++ pamSMF/doDSP.m
@@ -4,20 +4,47 @@
 % Electrical & Computer Engineering Department
 % Created by Alexander Salois
 clear; clc; close all;
-load('Pam_0001.mat')
 
-ref = real(InNode{1,2}.Signal.samples);
-rx = real(InNode{1,1}.Signal.samples);
-rx = rx - min(rx);
-rx = rx/max(rx);
+% load file
+loadName = sprintf('pam_snr_%02d_len_%04d_%04d',40,9,1)
+load(loadName)
 
+% rescale to work with pamdemod
+inSig = real(InNode{1,2}.Signal.samples);
+inSig = inSig*6 -3;
+outSig = real(InNode{1,1}.Signal.samples);
+outSig = outSig - min(outSig);
+outSig = outSig/max(outSig);
+outSig = outSig*6 -3;
 
+%%
+% delay = 0;
+% 
+% inSig = inSig(1:end-delay);
+% outSig = outSig(delay+1:end);
 
+M = 4;
+grabBit = log2(M)*2^pointsPerBit;
+startOut = 8*floor(grabBit/16);
+
+% grab one bit per symbol
+selectIn = inSig(4:grabBit:end);
+selectOut = outSig(startOut:grabBit:end);
+
+% demod
+bitsIn = pamdemod(selectIn,M);
+bitsOut = pamdemod(selectOut,M);
+
+% get BER
+[numWrong,ber] = biterr(bitsIn,bitsOut)
+
+sel = 1:2^10;
 figure()
 hold on
-plot(ref)
-plot(rx)
+plot(inSig(sel))
+plot(outSig(sel))
 hold off
-
-eyediagram(rx,2^pointsPerBit)
-eyediagram(ref,2^pointsPerBit)
+% 
+% %%
+% eyediagram(inSig,2^pointsPerBit)
+% eyediagram(outSig,2^pointsPerBit)
